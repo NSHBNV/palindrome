0311aced4559289b3af53b59fd6d06481e525446
Nikita Shabanov <nshbnv@gmail.com>
2022-02-17 16:28:41 +0300

    Realize isPalindrome method

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -2,15 +2,20 @@ public class Palindrome {
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++) {
             String s = args[i];
+            System.out.println(isPalindrome(s));
         }
-        System.out.println(reverseString("pizzeria"));
     }
 
-    public static String reverseString(String s){
+    public static String reverseString(String s) {
         String newString = "";
-        for(int i = s.length() - 1; i >= 0; i--){
+        for (int i = s.length() - 1; i >= 0; i--) {
             newString += s.charAt(i);
         }
         return newString;
+    }
+
+    public static boolean isPalindrome(String s) {
+        String newString = reverseString(s);
+        return s.equals(newString);
     }
 }
