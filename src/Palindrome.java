717848a7a58923282eba2b8ff481b7d127744e87
Nikita Shabanov <nshbnv@gmail.com>
2022-02-17 15:48:02 +0300

    Realization of reverseString

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,5 +1,16 @@
 public class Palindrome {
     public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            String s = args[i];
+        }
+        System.out.println(reverseString("pizzeria"));
+    }
 
+    public static String reverseString(String s){
+        String newString = "";
+        for(int i = s.length() - 1; i >= 0; i--){
+            newString += s.charAt(i);
+        }
+        return newString;
     }
 }
